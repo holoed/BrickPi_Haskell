47754e5f9f627cfafe2dffa5c300c0c0e483d2fd
Edmondo Pentangelo <edmondo@gmail.com>
2013-12-05 23:02:03 +0000

    Updated test to turn bot left and right

--- BrickPi.hs
+++ BrickPi.hs
@@ -2,13 +2,22 @@
 import Data.Char
 import BrickPi_Serial
 
-
+send :: Int -> [Int] -> IO ()
+send h xs = sequence_ (map (serialPutchar h) (map chr xs))
 
 main = do h <- serialOpen "/dev/ttyAMA0" 500000
           print (show h)
-          sequence_ (map (serialPutchar h) (map chr [1,218,4,3,132,28,50]))
+          print "Go Forward"
+          send h [1,218,4,3,132,28,50]
           getLine
-          sequence_ (map (serialPutchar h) (map chr [1,2,4,3,140,60,50]))
+          print "Go Backward"
+          send h [1,2,4,3,140,60,50]
+          getLine
+          print "Turn Left"
+          send h [1,226,4,3,140,28,50]
+          getLine
+          print "Turn Right"
+          send h [1,250,4,3,132,60,50]
           getLine
           serialClose h
           return ()
